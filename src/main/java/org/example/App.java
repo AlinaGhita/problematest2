1305178c06bf940e1c3b63a382b780b02ec44ebf
Alina Ghta <alinaaghita@gmail.com>
2025-02-16 12:34:03 +0200

    commit for PR

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        List<Package> packages = PackageViewer.viewPackages("packages.txt");
+        Map<String, Map<String, List<Package>>> groupedPackages = PackageOrganizer.groupPackages(packages);
+
+        List<Thread> threads = new ArrayList<>();
+
+        //aici m am ajutat de internet
+        for (Map.Entry<String, Map<String, List<Package>>> locationEntry : groupedPackages.entrySet()) {
+            for (Map.Entry<String, List<Package>> dateEntry : locationEntry.getValue().entrySet()) {
+                Thread thread = new Thread(new Delivery(locationEntry.getKey(), dateEntry.getKey(), dateEntry.getValue()));
+                threads.add(thread);
+                thread.start();
+            }
+        }
+
+        System.out.println("Total value of packages: " + Delivery.getTotalPackageValue());
+        System.out.println("Total revenue " + Delivery.getTotalRevenue());
     }
 }
--- src/main/java/org/example/Delivery.java
+++ src/main/java/org/example/Delivery.java
@@ -1,4 +1,49 @@
 package org.example;
 
-public class Delivery {
+import java.util.List;
+
+class Delivery implements Runnable {
+    private String location;
+    private String date;
+    private List<Package> packages;
+    private static int totalPackageValue = 0;
+    private static int totalRevenue = 0;
+
+    public Delivery(String location, String date, List<Package> packages) {
+        this.location = location;
+        this.date = date;
+        this.packages = packages;
+    }
+
+    public static int getTotalRevenue() {
+        return totalRevenue;
+    }
+
+    public static int getTotalPackageValue() {
+        return totalPackageValue;
+    }
+
+    @Override
+    public void run() {
+        int distance = 0;
+        distance = packages.get(0).distance;
+        int totalValue = 0;
+        for (Package p : packages) {
+            totalValue += p.value;
+            int revenue = distance;
+
+            System.out.println("[Delivering for " + location + " and date " + date + " in " + distance + " seconds]");
+
+            try {
+                Thread.sleep(distance * 5000);
+            } catch (InterruptedException e) {
+                e.getMessage();
+            }
+
+            totalPackageValue += totalValue;
+            totalRevenue += revenue;
+
+        }
+
+    }
 }
